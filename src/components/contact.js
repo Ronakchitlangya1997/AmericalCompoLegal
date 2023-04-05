c4a27174e1d1c40843a698e0bb08f0c837500b1c
Ronak Chitlangya <chitlangyaronak1997@gmail.com>
2023-04-05 19:54:04 +0530

    API Updated

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import axios from "axios";
+import {useState} from "react";
 
 export function Contact() {
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+    
+    const submit = async e => {
+        e.preventDefault();
+
+        const user = {
+            firstname: firstname,
+            lastname: lastname,
+            email:email,
+            phone:phone,
+            message:message
+          };
+        console.log(user)
+        const {data} = await axios.post('http://13.233.131.230/leadsamerican-compo-legel/', user ,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
 
     return (
         <div className="Section4 d-flex">
@@ -20,13 +43,34 @@ export function Contact() {
             </div>
             <div className="Section4_2">
                 <p className="Section4_2_heding1">Make Your Voice Heard</p>
-                <form className="Auth-form">
+                <form className="Auth-form" onSubmit={submit}>
                     <div className='Section4_2_inputbox d-flex'>
-                        <input className="Section4_2_input" type="text" id="fname" name="fname" placeholder='First Name'/>
-                        <input className="Section4_2_input" type="text" id="lname" name="lname" placeholder='Last Name'/>
-                        <input className="Section4_2_input" type="text" id="email" name="email" placeholder='Email'/>
-                        <input className="Section4_2_input" type="text" id="phone" name="phone" placeholder='Phone'/>
-                        <textarea className="Section4_2_input messbox" type="text" id="message" name="message" placeholder='Type Your Message Here'/>
+                        <input className="Section4_2_input" type="text" 
+                        id="firstname" name="firstname" 
+                        placeholder='First Name' 
+                        value={firstname}
+                        required
+                        onChange={e => setFirstname(e.target.value)}/>
+                        <input className="Section4_2_input" type="text" id="lastname" 
+                        name="lastname" placeholder='Last Name'
+                        value={lastname}
+                        required
+                        onChange={e => setLastname(e.target.value)}/>
+                        <input className="Section4_2_input" type="email" id="email" 
+                        name="email" placeholder='Email'
+                        value={email}
+                        required
+                        onChange={e => setEmail(e.target.value)}/>
+                        <input className="Section4_2_input" type="number" id="phone" 
+                        name="phone" placeholder='Phone'
+                        value={phone}
+                        required
+                        onChange={e => setPhone(e.target.value)}/>
+                        <textarea className="Section4_2_input messbox" type="text" id="message" 
+                        name="message" placeholder='Type Your Message Here'
+                        value={message}
+                        required
+                        onChange={e => setMessage(e.target.value)}/>
                     </div>
                     <button className="Section4_2_form_button" type="submit">
                         Submit
